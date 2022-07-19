c8f40d0425fa2ae5fd577e219087967fe5257ea1
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-19 11:25:13 +0200

    step 1 to 10 finished

---
+++ server/server.js
@@ -0,0 +1,36 @@
+// require express and http
+const express = require('express');
+const http = require('http');
+
+// We will use express and http to host our client
+// Define our application
+const app = express();
+// Give the path to our client
+const clientPath = `${__dirname}/../client`;
+// Use express to host the client
+app.use(express.static(clientPath));
+// Use http to serve the app that express provides
+const server = http.createServer(app);
+let port = 8080;
+// Get the server live
+server.listen(8080, () => {
+    console.log("server running on "+port);
+});
+// Require socket.io
+// The io variable is now the entry point of all the sockets connected to the server
+const io = require('socket.io')(server);
+
+// Making a connection from our client to our server
+/*
+io.on('connection', (socket) => {
+    console.log('someone connected');
+});
+*/
+
+// Connect multiple devices to our server
+let counter = 0;
+
+io.on('connection', (socket) => {
+    counter++;
+    console.log(counter+ ' someone connected');
+});
