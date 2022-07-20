8d8b055cecf24cc1ec524beded38b9b461444a12
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-20 17:01:21 +0200

    connect disconnect, client side

--- client/js/script.js
+++ client/js/script.js
@@ -1,3 +1,11 @@
 // Define socket
-const socket = io.connect();
+let socket = io();
 
+// built in event listeners connect and disconnect
+socket.on('connect', () => {
+    console.log('Connected to server.');
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected from server.');
+});
