20bdae1229ba737656065617d814aeeff68cba3e
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-22 15:18:36 +0200

    displaying users in a room Seeing the users in a list when they are in a  room, the name dissapears when they leave a room, getting the name when they post a message and when they send their location

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 
 const {newMessage, newLocationMessage} = require('./utils/message');
 const {realString} = require('./utils/realString');
+const {Users} = require('./utils/users');
 
 // We will use express and http to host our client
 // Define our application
@@ -23,6 +24,8 @@ server.listen(8080, () => {
 // The io variable is now the entry point of all the sockets connected to the server
 const io = require('socket.io')(server);
 
+let users = new Users();
+
 
 // Connect multiple devices to our server
 let counter = 0;
@@ -34,12 +37,16 @@ io.on('connection', (socket) => {
 
     socket.on('join', (params, callback) => {
         if(!realString(params.name) || !realString(params.room)){
-            callback('Name and room are required');
+            return callback('Name and room are required');
         }
 
-        console.log(socket.id);
         socket.join(params.room);
+        //if a user joins a room we remove them from any potential room they might still be in
+        users.removeUser(socket.id);
+        //add user to new room
+        users.addUser(socket.id, params.name, params.room);
 
+        io.to(params.room).emit('updateUsersList', users.getUserList(params.room));
         socket.emit('newMessage', newMessage('Admin', `Welcome to ${params.room}!`));
 
         socket.broadcast.to(params.room).emit('newMessage', newMessage('Admin', 'New user joined!'));
@@ -49,8 +56,12 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message, callback) => {
         console.log("createMessage", message);
-        //io, everyone connected, everyone gets this message even itself
-        io.emit('newMessage', newMessage(message.from, message.text));
+        let user = users.getUser(socket.id);
+
+        if(user && realString(message.text)){
+            //io, everyone connected to the specific room gets this message even the person who send it, if we do not specify the room it will be sent to all chat rooms.
+            io.to(user.room).emit('newMessage', newMessage(user.name, message.text));
+        }
         callback('This is the server:');
 
         // broadcast sends an event to everyone, except for the person that created the event
@@ -62,11 +73,21 @@ io.on('connection', (socket) => {
     })
 
     socket.on('createLocationMessage', (coords) => {
-        //emit to everybody including ourselves
-        io.emit('newLocationMessage', newLocationMessage('Admin', coords.lat, coords.lng))
+        let user = users.getUser(socket.id);
+
+        if(user){
+            //emit to everybody in a specific room including ourselves
+            io.to(user.room).emit('newLocationMessage', newLocationMessage(user.name, coords.lat, coords.lng))
+        }
     })
 
     socket.on('disconnect', () => {
         console.log('A user disconnected.');
+        let user = users.removeUser(socket.id);
+
+        if(user){
+            io.to(user.room).emit('updateUsersList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', newMessage('Admin', `${user.name} has left ${user.room} chat room.`));
+        }
     });
 });
